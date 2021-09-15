435fba41e876fd03a420a03db8975c0e0a202851
samuel borges <muka_brayan-borges@hotmail.com>
2021-09-14 21:20:17 -0300

    #0 - Created Project and tasks

---
+++ simpleTestAspNetApp/BLL/ArrayUtils.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace simpleTestApplication.BLL
+{
+    public static class ArrayUtils
+    {
+        public static int[] ReplaceValue(int[] numbers, int value, int substituteValue)
+        {
+            IEnumerable<int> EnumerabledValues = numbers;
+
+            foreach (var number in EnumerabledValues.Select((numbers, index) => new { index, numbers }))
+            {
+                if (numbers[number.index].Equals(value))
+                {
+                    numbers[number.index] = substituteValue;
+                    break;
+                }
+
+            }
+
+            return numbers;
+        }
+        public static int[] RemoveValue(int[] numbers, int removableValue)
+        {
+            return numbers.Where(value => value != removableValue).ToArray();
+        }
+        public static int AddArrayValues(int[] numbers)
+        {
+            int addedValue = 0;
+            foreach (int number in numbers)
+            {
+                addedValue += number;
+            }
+
+            return addedValue;
+        }
+
+        public static Dictionary<int, int[]> AddRandomArrayValues(int[] values)
+        {
+            Random random = new Random();
+
+            int firstRandomPosition = 0;
+            int secondRandomPosition = 0;
+
+            for (int i = 0; firstRandomPosition == secondRandomPosition; i++)
+            {
+                firstRandomPosition = random.Next(0, values.Length);
+                secondRandomPosition = random.Next(0, values.Length);
+            }
+
+            int firstNumber = values[firstRandomPosition];
+            int secondNumber = values[secondRandomPosition];
+
+            int addedNumbers = firstNumber + secondNumber;
+
+            Dictionary<int, int[]> resultMap = new Dictionary<int, int[]>();
+
+            resultMap.Add(addedNumbers, new int[] { firstNumber, secondNumber });
+
+            return resultMap;
+        }
+
+    }
+}
---
+++ simpleTestAspNetApp/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using simpleTestApplication.BLL;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace simpleTestApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        int[] OriginalNumbers = new int[7] { 7, 5, 3, 9, 6, 4, 1 };
+
+        public IActionResult Index()
+        {
+            //Tarefa 1
+            ViewBag.TaskOne = this.TaskOneManager();
+
+            //Tarefa 2
+            ViewBag.TaskTwo = this.TaskTwoManager();
+
+            //Tarefa 3
+            ViewBag.TaskThree = this.TaskThreeManager(new int[] { 2, 7, 11, 15 });
+            
+            return View();
+        }
+
+        private string TaskOneManager()
+        {
+            int[] numbers =  OriginalNumbers.ToArray();
+            int sumOfValues;
+
+            numbers = ArrayUtils.RemoveValue(ArrayUtils.ReplaceValue(numbers, 9, 5), 4);
+            sumOfValues = ArrayUtils.AddArrayValues(numbers);
+
+            return $"Lista original: [{String.Join(",", OriginalNumbers)}]. Nova lista: [{String.Join(",",numbers)}]. Soma dos valores da lista: {sumOfValues}";
+        }
+        private string TaskTwoManager()
+        {
+            return "O resultado da consulta será 85!";
+        }
+        private string TaskThreeManager(int[] values)
+        {
+            Dictionary<int, int[]> addedArrayValues;
+            int firstNumber;
+            int secondNumber;
+            int total;
+
+            addedArrayValues  = ArrayUtils.AddRandomArrayValues(values);
+            firstNumber  = (int)addedArrayValues.First().Value.GetValue(0);
+            secondNumber  = (int)addedArrayValues.First().Value.GetValue(1);
+            total = (int)addedArrayValues.First().Key;
+
+            return $"Lista de exemplo: [{String.Join(",", values)}], {firstNumber} + {secondNumber} resulta na soma de entrada: {total}! ";
+        }
+    }
+}
